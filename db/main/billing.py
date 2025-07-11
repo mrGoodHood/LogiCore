01daa3cc7de78395746dcf35bb3981c9aae96453
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-07-11 11:25:59 +0300

    get account balance func

--- db/main/billing.py
+++ db/main/billing.py
@@ -7,4 +7,11 @@ async def get_new_acc_number():
 
 async def get_acc_balance(user_id):
     async with async_session_maker() as session:
-        pass
+        statement = select(Contract.ID, Contract.Number, Contract.Balance).join(
+            Contract2User, Contract.ID == Contract2User.ContractID
+        ).where(
+            Contract2User.UserID == user_id
+        )
+        result = await session.execute(statement)
+        acc_id, acc_num, acc_balance = result.fetchone()
+        return acc_id, acc_num, acc_balance
