39d4184f4fcc3feafa9bd3c060398cde59b7f95a
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-07-12 00:26:49 +0300

    settings update

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 import os
-from logging import DEBUG
 
 from dotenv import load_dotenv
 
@@ -8,5 +7,11 @@ load_dotenv()
 DATA_PATH = "data"
 
 DEBUG = bool(os.getenv('DEBUG') == "True")
-
 DB_CONN = os.getenv('DB_CONN')
+DB_CREATE = bool(os.getenv('DB_CREATE') == 'True')
+SUGGEST_TOKEN = os.getenv('SUGGEST_TOKEN')
+SUGGEST_URL = os.getenv('SUGGEST_URL')
+USER_SECRET = os.getenv('USER_SECRET')
+USER_LIFESPAN = int(os.getenv('USER_LIFESPAN'))
+POST_URL = os.getenv('POST_URL')
+POST_API_TIMEOUT = int(os.getenv('POST_API_TIMEOUT'))
