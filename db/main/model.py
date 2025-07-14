6c5a3eefad7d49401a9e29f0f7ca4ff636f68cc9
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-07-14 22:03:19 +0300

    models update

--- db/main/model.py
+++ db/main/model.py
@@ -17,3 +17,25 @@ class ParsedAddressRow(BaseModel):
     place: str
     street: str | None = None
     house: str | None = None
+
+
+class ParsedAddressList(BaseModel):
+    address_list: list[ParsedAddressRow] = None
+
+
+class AddressRow(BaseModel):
+    """Строка адреса для валидации
+    """
+    rownum: int
+    address: str
+
+
+class AddressList(BaseModel):
+    """Список адресов для валидации отправляется в API https://otpravka.pochta.ru/specification#/nogroup-normalization_adress
+    """
+    address_list: list[AddressRow] = None
+
+
+class NameRow(BaseModel):
+    rownum: int
+    name: str
