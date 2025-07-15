7ba86cb9654a965d9082ed74d89fe69f161c9671
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-07-15 11:12:03 +0300

    Contract class for db shema

---
+++ db/schema/billing.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import mapped_column as mcol
+
+
+class ContractType(StrEnum):
+    MASTER = 'MASTER'  # основной тип аккаунта создается по дефолту всем
+    SHIPMENT = 'SHIPMENT'  # аккаунт для операций связанных с отправкой
+    COD = 'COD'  # аккаунт для наложенных платежей
+    RETURN = 'RETURN'  # аккаунт для возвратов
+
+
+class Contract(Base):
+    __tablename__ = "bill_contract"
+    ID = mcol(BIGINT, primary_key=True, autoincrement=True, index=True)
+    ParentID = mcol(BIGINT, index=True)
+    Type = mcol(ENUM(ContractType, name='contract_type_enum'), default='MASTER', nullable=False)
+    Number = mcol(VARCHAR(length=20), unique=True, nullable=False, index=True)
+    Balance = mcol(NUMERIC(18, 4), nullable=False, default=0)
+    IsActive = mcol(BOOLEAN, nullable=False, default=True)
+    Note = mcol(TEXT)
+    Created = mcol(TIMESTAMP(timezone=False), server_default=func.now(), nullable=False)
+    Closed = mcol(TIMESTAMP(timezone=False))
+    IsDeleted = mcol(BOOLEAN, nullable=False, default=False)
