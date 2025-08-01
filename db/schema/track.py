345b1eaa69df1a51bdababea3459764e4fdc77b0
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-08-01 21:13:28 +0300

    TracMailCategory class

--- db/schema/track.py
+++ db/schema/track.py
@@ -22,3 +22,12 @@ class TrackBarcodeTicket(Base):
     ID = mcol(BIGINT, primary_key=True, autoincrement=True, index=True)
     BarcodeID = mcol(ForeignKey(Barcode.ID, ondelete="CASCADE"), nullable=False)
     TicketID = mcol(ForeignKey(TrackTicket.ID, ondelete="CASCADE"), nullable=False)
+
+
+class TrackMailCategory(Base):
+    __tablename__ = "track_mail_category"
+    ID = mcol(SMALLINT, primary_key=True, autoincrement=True, index=True)
+    Value = mcol(VARCHAR(length=256), nullable=False)
+    Description = mcol(VARCHAR(length=256), nullable=False)
+    VerboseName = mcol(VARCHAR(length=256))
+    IsCODExists = mcol(BOOLEAN, nullable=False)
