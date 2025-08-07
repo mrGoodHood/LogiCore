f3143119758d7c02046999cce37d2a961c90fe6e
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-08-08 01:23:17 +0300

    get method

--- router/v1/endpoints/batch.py
+++ router/v1/endpoints/batch.py
@@ -1,3 +1,5 @@
+from types import NoneType
+
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/batch")
@@ -12,3 +14,19 @@ def size_validator(value: int) -> int:
     if value <= 0:
         raise ValueError("Размер страницы не может быть меньше или равен нулю.")
     return value
+
+
+@router.get(
+    path="",
+    summary="Получение всех партий.",
+    description='''
+    Возвращает все партии пользователя.
+    ''',
+    response_model=None,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Bad Request.",
+            "model": None,
+        },
+    },
+)
