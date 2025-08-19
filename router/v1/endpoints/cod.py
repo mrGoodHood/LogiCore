c9689a20a08c4bfd6029ced5712e4c0477dcd16b
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-08-19 23:26:02 +0300

    cod registry get method fix

--- router/v1/endpoints/cod.py
+++ router/v1/endpoints/cod.py
@@ -7,3 +7,15 @@ router = APIRouter(prefix="/cod")
     description="Получить реестр наложенных платежей",
     response_model=list[CODRegistryTableRepresentation],
 )
+async def get_cod_registry(
+        cod_registry_getter: CODRegistryGetter = Depends(get_cod_registry_getter),
+        user: User = Depends(current_active_user),
+) -> Union[Response, list[CODRegistryTableRepresentation]]:
+    user_settings = await get_contract_settings(user.id)
+    cod_fee = user_settings.get("cod_fee")
+
+    if not cod_fee:
+        raise HTTPException(detail=f"cod_fee не определен у пользователя {user.id}", status_code=404)
+
+    registries = await cod_registry_getter.get(user.id, Decimal(cod_fee))
+    return registries
