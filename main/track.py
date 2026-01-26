ab49efcaec92fd5e4a65b4aa9b40cf30de8ceebb
Andrey Zhidkov <mr.goodhood@gmail.com>
2026-01-26 23:45:40 +0300

    TrackSummarySort

--- main/track.py
+++ main/track.py
@@ -3,3 +3,9 @@ class TrackSummaryFilter(StrEnum):
     IS_HOT = "is_hot"
     IS_NOT_HOT = "is_not_hot"
     IS_HANGING = "is_hanging"
+
+
+class TrackSummarySort(StrEnum):
+    TRACK_STATUS = "track_status"
+    BATCH_SENT_DATE = "batch_sent_date"
+
