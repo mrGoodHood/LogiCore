465a50d6c23c501a977ce7f6a3d4cdf37a820706
Andrey Zhidkov <mr.goodhood@gmail.com>
2026-01-26 23:54:37 +0300

    set_track_summary_filter method

--- main/track.py
+++ main/track.py
@@ -9,3 +9,8 @@ class TrackSummarySort(StrEnum):
     TRACK_STATUS = "track_status"
     BATCH_SENT_DATE = "batch_sent_date"
 
+
+def set_track_summary_filter(query: Select, query_filter: Optional[TrackSummaryFilter]) -> Select:
+    if query_filter == TrackSummaryFilter.IS_RETURNED:
+        return query.where(TrackSummary.IsReturned == True)
+    #TODO
