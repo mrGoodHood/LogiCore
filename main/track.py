70da1baa1169489f51966bae9518cd8d3521ac6e
Andrey Zhidkov <mr.goodhood@gmail.com>
2026-01-28 23:59:21 +0300

    set_track_summary_filter method update

--- main/track.py
+++ main/track.py
@@ -13,4 +13,10 @@ class TrackSummarySort(StrEnum):
 def set_track_summary_filter(query: Select, query_filter: Optional[TrackSummaryFilter]) -> Select:
     if query_filter == TrackSummaryFilter.IS_RETURNED:
         return query.where(TrackSummary.IsReturned == True)
+    elif query_filter == TrackSummaryFilter.IS_HOT:
+        return query.where(TrackSummary.IsHot == True)
+    elif query_filter == TrackSummaryFilter.IS_NOT_HOT:
+        return query.where(TrackSummary.IsHot == False)
+    elif query_filter == TrackSummaryFilter.IS_HANGING:
+        month_ago = datetime.today() - relativedelta(months=+1)
     #TODO
